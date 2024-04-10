b1ccfbde60d22b4be37f1fada976222ed84a0929
Capuchill <orutvictoria05780@gamil.com>
2024-04-10 18:39:01 -0500

    informe voluntarios

--- src/main/java/com/ayudas/ong/repositories/models/dtos/VoluntarioAdminDTO.java
+++ src/main/java/com/ayudas/ong/repositories/models/dtos/VoluntarioAdminDTO.java
@@ -5,14 +5,19 @@ import com.ayudas.ong.repositories.enums.TipoVoluntario;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter @Setter
 public class VoluntarioAdminDTO extends PersonaDTO{
 
     private TipoVoluntario tipo;
     private String sede;
 
+    public VoluntarioAdminDTO() {
+        super(); // Llama al constructor de la clase padre si es necesario
+    }
 }
